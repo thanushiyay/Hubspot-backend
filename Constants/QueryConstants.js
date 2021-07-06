4ec66f0a32f06bfc1b91753a84305c94fd91c21f
thanushiyay <thanuyt@gmail.com>
2021-07-06 22:18:21 +0530

    Error Changes

--- Constants/QueryConstants.js
+++
@@ -1,9 +0,0 @@
-const queryConstants = () => {
-    const users = {
-        SAVE_USER: 'INSERT INTO USERS(name, email, password) VALUE (?,?,?)',
-        GET_USER: 'SELECT * FROM USERS WHERE email = ?',
-        GET_USER_PRODUCTS : 'select users.id, users.name, users.email, products.name as P_name,products.id as P_id, category.id as C_id, category.name as C_name from users Inner join products on users.id = products.user_id Left join category on products.category_id = category.id'
-    }
-    return {users}
-}
-module.exports = { queryConstants }
--- Controllers/Hubspot.js
+++ Controllers/Hubspot.js
@@ -1,44 +1,51 @@
-const request = require('request');
 const express = require('express');
+const request = require('request-promise');
 const router = express.Router();
 
 router.get('/getAllContacts', async(req, res, next) => {
     try {
+        console.log('Get All Contacts');
         const endpoint = 'contacts/v1/lists/all/contacts/all';
-        const contacts = await request({
-            method: 'GET',
-            url: `https://api/hubapi.com/${endpoint}`,
-            qs: {
-                hapikey: "8546fa0c-6010-4ad0-9437-58df1a2e4437"
-            },
-            json: true
-        })
-        let userContact = [];
-        [...contacts.contacts].map(data => {
+        const contact = await request({
+          method: 'GET',
+          url: `https://api.hubapi.com/${endpoint}`,
+          qs: {
+            hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437',
+            count: 100
+          },
+          json: true
+        });
+        let userContact=[];
+        [...contact.contacts].map(data => {
             userContact.push(data.properties);
+            console.log("usercontact",userContact);
         })
         res.status(200).send({"Contacts": userContact});
-    } catch (error) {
-        
-    }
+      } catch (error) {
+        console.log('error ', error);
+      }
 })
 
 router.post('/createContacts', async(req, res, next) => {
     try {
+        console.log('Create New Contact');
         const endpoint = 'contacts/v1/contact';
-        console.log(req.body.userContact);
-        const contacts = await request({
-            method: 'POST',
-            url: `https://api/hubapi.com/${endpoint}`,
-            qs: {
-                hapikey: "8546fa0c-6010-4ad0-9437-58df1a2e4437"
-            },
-            body: req.body.userContact,
-            json: true
-        })
-    } catch (error) {
-        
-    }
+        const createcontact = await request({
+          method: 'POST',
+          url: `https://api.hubapi.com/${endpoint}`,
+          qs: {
+            hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437'
+          },
+          body: req.body.userContact,
+          json: true
+        });
+        res.status(200).send({"Contacts": createcontact});
+      } catch (error) {
+        console.log('error ', error);
+      }
 })
 
 module.exports = router;
+
+
+
