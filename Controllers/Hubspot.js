f1451408aa29bffa97d8ef18e5817c770e622f4e
Surya Prasanth S <surya.15cs@kct.ac.in>
2021-07-10 18:43:40 +0530

    TableCreation

--- Controllers/Hubspot.js
+++ Controllers/Hubspot.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const request = require('request-promise');
 const router = express.Router();
-
+const api = 'https://api.hubapi.com';
 router.get('/getAllContacts', async(req, res, next) => {
     try {
         console.log('Get All Contacts');
-        const endpoint = 'contacts/v1/lists/all/contacts/all';
+        const endpoint = '/contacts/v1/lists/all/contacts/all';
         const contact = await request({
           method: 'GET',
-          url: `https://api.hubapi.com/${endpoint}`,
+          url: api + endpoint,
           qs: {
             hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437',
             count: 100
@@ -29,10 +29,10 @@ router.get('/getAllContacts', async(req, res, next) => {
 router.post('/createContacts', async(req, res, next) => {
     try {
         console.log('Create New Contact');
-        const endpoint = 'contacts/v1/contact';
+        const endpoint = '/contacts/v1/contact';
         const createcontact = await request({
           method: 'POST',
-          url: `https://api.hubapi.com/${endpoint}`,
+          url: api + endpoint,
           qs: {
             hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437'
           },
@@ -45,6 +45,25 @@ router.post('/createContacts', async(req, res, next) => {
       }
 })
 
+router.get('/checkContacts/:id', async(req, res, next) => {
+  try {
+    console.log(req.params.id);
+    const endpoint = '/contacts/v1/contact/email/';
+    console.log(api + endpoint + req.params.id + '/profile')
+    const getContacts = await request({
+      method: 'GET',
+      url: api + endpoint + req.params.id + '/profile',
+      qs: {
+        hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437'
+      },
+      json: true
+    });
+    console.log(getContacts);
+    res.status(200).send({message: getContacts});
+  } catch (error) {
+    res.status(200).send({message: "failure"});
+  }
+})
 module.exports = router;
 
 
---
+++ Controllers/HubspotTable.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const request = require('request-promise');
+const router = express.Router()
+
+const api = 'https://api.hubapi.com/hubdb';
+
+router.post('/add', async(req, res, next) => {
+    try {
+        const endpoint = '/api/v2/tables/5249552/rows';
+        let requestBody = {
+            "values": req.body.values
+        }
+        request({
+            method: 'POST',
+            url: api + endpoint,
+            qs: {
+                hapikey: '8546fa0c-6010-4ad0-9437-58df1a2e4437'
+              },
+            json: true,
+            body: requestBody
+        }).then(success => {
+            console.log('Row Inserted');
+            res.status(200).send({message: "Row Inserted"});
+        }).catch(error => {
+            console.log('error', error.message);
+            res.status(200).send({message: "Error occured"});
+            // throw new Error(error)
+        })
+    } catch (error) {
+        console.log("error", error);
+    }
+})
+
+
+module.exports = router;
