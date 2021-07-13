8250e62b7e0e2a7e873893dd33e3dfe6f1ce7330
thanushiyay <thanuyt@gmail.com>
2021-07-13 11:02:12 +0530

    table id changed

--- Controllers/HubspotTable.js
+++ Controllers/HubspotTable.js
@@ -6,7 +6,8 @@ const api = 'https://api.hubapi.com/hubdb';
 
 router.post('/add', async(req, res, next) => {
     try {
-        const endpoint = '/api/v2/tables/5249643/rows';
+        const endpoint = '/api/v2/tables/5249703/rows';
+        console.log(req.body.values);
         let requestBody = {
             "values": req.body.values
         }
